53d95439d565faf8d8d709cf61a598183b542e83
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2025-05-27 20:00:49 +0200

    Bump papi (#687)

--- packages/ui/src/hooks/useGetIdentity.tsx
+++ packages/ui/src/hooks/useGetIdentity.tsx
@@ -41,39 +41,45 @@ export const useGetIdentity = () => {
 
       const identity = await ctx.pplApi.query.Identity.IdentityOf.getValue(addressToUse, {
         at: 'best'
-      }).then((res) => {
-        const id: IdentityInfo = { judgements: [], sub }
-        let val: WesPplQueries['Identity']['IdentityOf']['Value'] | undefined
-
-        if (Array.isArray(res)) {
-          val = res[0]
-        } else {
-          val = res
-        }
-
-        if (!val) return
-
-        val.judgements.forEach(([, judgement]) => {
-          id.judgements.push(judgement.type)
-        })
-        Object.entries(val.info || {}).forEach(([key, value]) => {
-          if ((value as IdentityData)?.type !== 'None') {
-            // console.log('key', JSONprint(key));
-            // console.log('value', JSONprint(value));
-            const text = (value as IdentityData)?.value as FixedSizeBinary<2> | undefined
-            if (text) {
-              id[key] = text.asText()
-            }
-          }
-        })
-
-        if (id.judgements.length === 0 && id.sub === undefined && Object.keys(id).length === 2) {
-          // there's no identity
-          return undefined
-        }
-
-        return id
       })
+        .then((res) => {
+          const id: IdentityInfo = { judgements: [], sub }
+          let val: WesPplQueries['Identity']['IdentityOf']['Value'] | undefined
+
+          if (Array.isArray(res)) {
+            val = res[0]
+          } else {
+            val = res
+          }
+
+          if (!val) return
+
+          val.judgements.forEach(([, judgement]) => {
+            id.judgements.push(judgement.type)
+          })
+          Object.entries(val.info || {}).forEach(([key, value]) => {
+            if ((value as IdentityData)?.type !== 'None') {
+              // console.log('key', JSONprint(key));
+              // console.log('value', JSONprint(value));
+              const text = (value as IdentityData)?.value as FixedSizeBinary<2> | undefined
+              if (text) {
+                id[key] = text.asText()
+              }
+            }
+          })
+
+          if (id.judgements.length === 0 && id.sub === undefined && Object.keys(id).length === 2) {
+            // there's no identity
+            return undefined
+          }
+
+          return id
+        })
+        .catch((e) => {
+          console.error('Error getting identity')
+          console.error(e)
+          return undefined
+        })
 
       return identity
     },
